c029e82c341a0cb48d72510c344696d3e5a4c4ad
Mike Dion <mike@becomeindelible.com>
2022-04-26 08:47:03 -0400

    fixes CSS issues

--- js/app.js
+++ js/app.js
@@ -1,7 +1,22 @@
 $(document).ready(function () {
   $(".international-toggle").click(function (e) {
     e.preventDefault();
-    $("#international-address").toggleClass("hide");
+    $("#state, #province, #zip, #postal-code").toggleClass("hide");
+
+    // Invert the disabled & required settings
+    $("#state select, #province select, #zip input, #postal-code input").prop(
+      "disabled",
+      function (i, v) {
+        return !v;
+      }
+    );
+    $("#state select, #province select, #zip input, #postal-code input").prop(
+      "required",
+      function (i, v) {
+        return !v;
+      }
+    );
+
     $(".international-toggle").toggleClass("hide");
   });
 
